54f93e2b50e65d096db4ba8f12dda53bbe63ca34
AzrielFabito <91511363+AzrielFabito@users.noreply.github.com>
2021-09-29 10:16:00 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,6 @@
 #The following codes and functions are used to create a program that stores and 
 #caches a matrix and its inverse. 
 #The first function is called makeCacheMatrix and it creates a unique matrix
-
-
 makeCacheMatrix <- function(x = matrix()) {
     invma <- NULL                                       
   set <- function(y) {
@@ -18,12 +16,9 @@ makeCacheMatrix <- function(x = matrix()) {
        get = get,
        setinvma = setinvma,
        getinvma = getinvma)}
-
 #The codes and the functions computes the inverse of the specific "matrix" which is created by the first function makeCacheMatrix. 
 #The second function is called the cacheSolve and it calculates the inverse of the matrix
 #If the inverse of the matrix has been calculated, the function cacheSolve must retrieve it from the cache.
-
-
 cacheSolve <- function(x, ...) {                
   invma <- x$getinvma()
   if (!is.null(invma)) {                        
