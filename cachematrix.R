0fa667c17b4b0653fcbb57e5b16337a49822aa2e
AzrielFabito <91511363+AzrielFabito@users.noreply.github.com>
2021-09-29 10:29:48 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,29 +2,30 @@
 #caches a matrix and its inverse. 
 #The first function is called makeCacheMatrix and it creates a unique matrix
 makeCacheMatrix <- function(x = matrix()) {
-    invma <- NULL                                       
-  set <- function(y) {
-          x <<- y
-          invma <<- NULL}
-  get <- function() x                                 
-  setinverse <- function(inverse) invma <<- inverse
-  getinverse <- function() {
-                            inver<-ginvma(x)
-                            inver%*%x
-                            }                         
+    p <- NULL
+  set <- function(b) {
+          t <<- b
+          p <<- NULL
+  }
+  get <- function() t
+  setinverse <- function(inverse) p <<- inverse
+  getinverse <- function() p
   list(set = set,
        get = get,
-       setinvma = setinvma,
-       getinvma = getinvma)}
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
 #The codes and the functions computes the inverse of the specific "matrix" which is created by the first function makeCacheMatrix. 
 #The second function is called the cacheSolve and it calculates the inverse of the matrix
 #If the inverse of the matrix has been calculated, the function cacheSolve must retrieve it from the cache.
-cacheSolve <- function(x, ...) {                
-  invma <- x$getinvma()
-  if (!is.null(invma)) {                        
-          message("loading....getting the data")
-          return(invma)}                            
-  data <- x$get()
-  invma <- solve(data, ...)
-  x$setinvma(invma)
-  invma}                                        
+cacheSolve <- function(t, ...) {
+  p <- x$getinverse()
+  if (!is.null(p)) {
+          message("Loading cache data")
+          return(p)
+  }
+  data <- t$get()
+  p <- solve(data, ...)
+  t$setinverse(p)
+  p
+}
